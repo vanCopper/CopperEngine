aa5b69dcdc4cabe83dfe05b4ab7dee76ab0e483e
vanCopper <vancopper@foxmail.com>
2022-06-02 15:09:01 +0800

    Modified EngineEditor

--- CopperEngine/src/Editor/EngineEditor.cpp
+++ CopperEngine/src/Editor/EngineEditor.cpp
@@ -1,2 +1,22 @@
 #include "CopperPCH.h"
 #include "EngineEditor.h"
+
+void Copper::EngineEditor::Initialize(CopperEngine* Engine)
+{
+
+}
+
+void Copper::EngineEditor::Start()
+{
+
+}
+
+void Copper::EngineEditor::ShutDown()
+{
+
+}
+
+void Copper::EngineEditor::Tick()
+{
+
+}
--- CopperEngine/src/Editor/EngineEditor.h
+++ CopperEngine/src/Editor/EngineEditor.h
@@ -1,10 +1,19 @@
 #pragma once
 
 #include "Runtime/Core/Core.h"
+#include "Runtime/Engine.h"
 
 namespace Copper
 {
 	class COPPER_API EngineEditor
 	{
+	public:
+		void Initialize(CopperEngine* Engine);
+		
+		void Start();
+		void ShutDown();
+
+	private:
+		void Tick();
 	};
 }
